865e702e7dc336c7c50268e04a050a09acaf6fc0
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-03-18 08:09:48 -0300

    Adicionamos a Tag Gato

---
+++ NossoProjeto/Gato.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const Gato = () =>{
+    return (
+        <View>
+            <Text> Meu nome é Gabriel.</Text>
+        </View>
+    );
+}
+export default Gato;
