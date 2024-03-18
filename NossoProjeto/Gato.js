ac2059e670fdf55cec0142c0250207ef01173d82
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-03-18 09:24:48 -0300

    texto da idade no singular e no plural

--- NossoProjeto/Gato.js
+++ NossoProjeto/Gato.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+
 const Gato = ({nome,idade}) =>{
+    const texto = idade > 1 ? "anos" : "ano";
     return (
         <View>
             <Text> Nome: {nome}</Text>
-            <Text> Idade: {idade} anos </Text>
+            <Text> Idade: {idade} {texto} </Text>
             
         </View>
     );
