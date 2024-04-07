90d8df5e315c80f04f640bde1eeceb0a2473c127
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2024-04-07 13:21:16 -0300

    Adicionando cor de fundo

--- Atividade-Figma/App.js
+++ Atividade-Figma/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<LinearGradient
+        colors={['#1A191A', 'transparent']}
+        style={styles.background}
+      />
     </View>
   );
 }
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9C56D3',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 3500,
   },
 });
