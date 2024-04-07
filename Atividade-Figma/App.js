5bfd53da9de4ad69a343663a62a7e67c1b3a6cd9
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2024-04-07 14:32:09 -0300

    Adicionando logo

--- Atividade-Figma/App.js
+++ Atividade-Figma/App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-<LinearGradient
+ 
+  <View style={styles.container}>
+           <LinearGradient
         colors={['#1A191A', 'transparent']}
         style={styles.background}
       />
-    </View>
+      <View style={styles.logo}>
+     <Image style={styles.imgLogo}  source={require('./assets/Image/logo_Instudo.png')} />
+      </View>
+ 
+ 
+    
+
+  </View>
+   
+
   );
 }
 
@@ -25,6 +35,20 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
-    height: 3500,
+    height: 4500,
+  },
+  logo: {
+    position: 'absolute',
+    top: 50,
+    left: 40,
+    height: 150,
+    width: 155,
+
+  },
+
+  imgLogo: {
+    height: 140,
+    width: 305,
+    objectFit: 'contain'
   },
 });
