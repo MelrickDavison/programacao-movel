d99822dc34ddb76c4617f9c52d87634b287570e4
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-07-08 11:19:42 -0300

    Adicionando imports das fontes

---
+++ Fonts/scripts/App.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+
+//Só vai utilizar uma vez
+import {useFonts} from 'expo-font' 
+import * as SplashScreen from 'expo-splash-screen';
+
+//Um import para cada fonte
+import {Inter_900Black} from '@expo-google-fonts/inter' //https://github.com/expo/google-fonts
+import { LaBelleAurore_400Regular } from '@expo-google-fonts/la-belle-aurore'
+import { useState } from 'react'
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
