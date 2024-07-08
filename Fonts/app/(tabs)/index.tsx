855ea34a785532c8daa6a89416ec1d4b6b0f0ace
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-07-08 11:26:33 -0300

    Modificando localidade index

--- Fonts/app/(tabs)/index.tsx
+++ Fonts/app/(tabs)/index.tsx
@@ -1,70 +1,68 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
+import React, {useState, useEffect} from 'react'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+//Só vai utilizar uma vez
+import {useFonts} from 'expo-font' 
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function HomeScreen() {
+//Um import para cada fonte
+import {Inter_900Black} from '@expo-google-fonts/inter' //https://github.com/expo/google-fonts
+import { LaBelleAurore_400Regular } from '@expo-google-fonts/la-belle-aurore'
+
+
+SplashScreen.preventAutoHideAsync();
+
+
+export default function App() {
+     //Fazer o import de cada fonte
+   const [loaded, error] = useFonts({
+    Inter_900Black,
+    LaBelleAurore_400Regular
+  });
+
+//Controle da Splash Screen
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync(); //Se a fonte for carregada ou tiver um erro vai esconder a splash screen
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles .container}>
+      <Text style={styles.subtitulo} >Welcome to</Text>
+      <Text style={styles.titulo} >Instudo</Text>
+    </View>
   );
+  
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+    container: {
+      marginTop: 50,
+      borderWidth: 2,
+      borderColor: 'blue',
+      borderStyle: 'dashed',
+      backgroundColor: '#000'
+  
+    },
+    
+    subtitulo: {
+      color: "#FFF06A",
+      marginTop: 50,
+      fontSize: 30,
+      textAlign: 'center',
+      fontFamily: 'LaBelleAurore_400Regular'
+    },
+  
+     titulo: {
+      textAlign: 'center',
+      fontFamily: 'Inter_900Black',
+      fontSize: 70,
+      color: '#fff'
+    }
+  });
