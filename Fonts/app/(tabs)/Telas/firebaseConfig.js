1d54f30ce87767c99d714cfae71b496586a22867
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2024-08-05 07:47:24 -0300

    configuracoes do firebase

---
+++ Fonts/app/(tabs)/Telas/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvAJIaQ-9hxFlI0ZXoChACGFIQXg8IKvs",
+  authDomain: "instudo-com-voce.firebaseapp.com",
+  projectId: "instudo-com-voce",
+  storageBucket: "instudo-com-voce.appspot.com",
+  messagingSenderId: "776660738382",
+  appId: "1:776660738382:web:0c82403f21bcbbe8817e1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
