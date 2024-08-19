85191b684d6e417f6e1b70c40f92f3672ddae101
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-08-19 09:30:26 -0300

    Implementando logoff

--- Fonts/app/(tabs)/Telas/home.js
+++ Fonts/app/(tabs)/Telas/home.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View} from 'react-native';
-import { auth } from '../../../firebaseConfig'
+import { useRoute } from '@react-navigation/native';
+import { signOut } from 'firebase/auth';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { auth } from '../../../firebaseConfig';
 
 export default function App() {
   const user = auth.currentUser;
+  const router = useRoute();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    router.replace('/(tabs)/Telas/telaLogin')  
+  }
   return (
     <View style={styles.container}>
       <Text>{user.email}, bem-vindo</Text>
+      <Button onPress={handleLogout} title={'Sair'}></Button>
     </View>
   );
 }
